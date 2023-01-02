fc99d92a74bc07644dd937d15d717bd729a513eb
supernoob1012 <felixmartel.no@gmail.com>
2023-01-02 12:37:16 +0330

    add sources

--- contracts/eth/market.sol
+++ contracts/eth/market.sol
@@ -1,1 +1,5 @@
-// https://docs.alchemy.com/docs/how-to-build-an-nft-marketplace-from-scratch
+// https://github.com/OMGWINNING/NFT-Marketplace-Tutorial/blob/master/contracts/NFTMarketplace.sol
+// https://github.com/Markkop/nft-marketplace/tree/main/contracts
+// https://github.com/fjun99/nftmarketplace/tree/main/contracts
+// https://trufflesuite.com/guides/nft-marketplace/#build-the-marketplace-contract
+// https://github.com/kofkuiper/kuiper-nft-marketplace
